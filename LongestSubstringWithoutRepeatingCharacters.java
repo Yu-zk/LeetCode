7738263db87987148f4f24be08a4150e1be64bb1
Ben <y1999zk@gmail.com>
2019-01-16 23:29:07 +0000

    Add files via upload

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -1,9 +1,10 @@
 public class LongestSubstringWithoutRepeatingCharacters {
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring1(String s) {
         int max=0;
+        if (s.length()==1) return 1;
         for (int i = 0; i < s.length()-1; i++) {
-            int n=0;
-            String a="";
+            int n=1;
+            String a=Character.toString(s.charAt(i));
             for (int j = i+1; j <s.length(); j++) {
                 if (a.contains(Character.toString(s.charAt(j)))){
                     break;
@@ -14,12 +15,28 @@ public class LongestSubstringWithoutRepeatingCharacters {
             }
             if (n>max) max=n;
         }
-
         return max;
+    }
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
         LongestSubstringWithoutRepeatingCharacters l =new LongestSubstringWithoutRepeatingCharacters();
-        System.out.println(l.lengthOfLongestSubstring("wertyuiopasdfghjkl"));
+        System.out.println(l.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(l.lengthOfLongestSubstring("bbbbb"));
+        System.out.println(l.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(l.lengthOfLongestSubstring("aaaaaaaaaaaaaaabvc"));
+        System.out.println(l.lengthOfLongestSubstring("pwwgehgehkew"));
+        System.out.println(l.lengthOfLongestSubstring(""));
+        System.out.println(l.lengthOfLongestSubstring("b"));
     }
 }
