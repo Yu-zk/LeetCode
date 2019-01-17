9e740ff028cd709ba30e79d17c218c49e775f6f1
Ben <y1999zk@gmail.com>
2019-01-17 22:55:51 +0000

    Add files via upload

--- LongestSubstringWithoutRepeatingCharacters.java
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+import java.util.Set;
 public class LongestSubstringWithoutRepeatingCharacters {
     public int lengthOfLongestSubstring1(String s) {
         int max=0;
@@ -18,13 +20,18 @@ public class LongestSubstringWithoutRepeatingCharacters {
         return max;
     }
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length(), ans = 0;
-        int[] index = new int[128]; // current index of character
-        // try to extend the range [i, j]
-        for (int j = 0, i = 0; j < n; j++) {
-            i = Math.max(index[s.charAt(j)], i);
-            ans = Math.max(ans, j - i + 1);
-            index[s.charAt(j)] = j + 1;
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))){
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            }
+            else {
+                set.remove(s.charAt(i++));
+            }
         }
         return ans;
     }
@@ -32,11 +39,11 @@ public class LongestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
         LongestSubstringWithoutRepeatingCharacters l =new LongestSubstringWithoutRepeatingCharacters();
         System.out.println(l.lengthOfLongestSubstring("abcabcbb"));
-        System.out.println(l.lengthOfLongestSubstring("bbbbb"));
-        System.out.println(l.lengthOfLongestSubstring("pwwkew"));
-        System.out.println(l.lengthOfLongestSubstring("aaaaaaaaaaaaaaabvc"));
-        System.out.println(l.lengthOfLongestSubstring("pwwgehgehkew"));
-        System.out.println(l.lengthOfLongestSubstring(""));
-        System.out.println(l.lengthOfLongestSubstring("b"));
+//        System.out.println(l.lengthOfLongestSubstring("bbbbb"));
+//        System.out.println(l.lengthOfLongestSubstring("pwwkew"));
+//        System.out.println(l.lengthOfLongestSubstring("aaaaaaaaaaaaaaabvc"));
+//        System.out.println(l.lengthOfLongestSubstring("pwwgehgehkew"));
+//        System.out.println(l.lengthOfLongestSubstring(""));
+//        System.out.println(l.lengthOfLongestSubstring("b"));
     }
 }
