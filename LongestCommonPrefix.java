bf59a375e0a516a3b022de96acad6bc974b2d60f
Ben <y1999zk@gmail.com>
2019-01-23 21:26:40 +0000

    Add files via upload

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,16 +1,25 @@
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-
         String s="";
+        if (strs.length==0) return "";
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c=strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if ((i<strs[j].length() && strs[j].charAt(i)!=c) || i==strs[j].length()){
+                    return s;
+                }
+            }
+            s=s+c;
+        }
         return s;
     }
 
     public static void main(String[] args) {
         LongestCommonPrefix l = new LongestCommonPrefix();
-        String[] s = new String[3];
-        s[0]="flower";
-        s[1]="flow";
-        s[2]="flight";
+        String[] s = new String[2];
+        s[0]="aa";
+        s[1]="a";
+//        s[2]="flight";
         System.out.println(l.longestCommonPrefix(s));
     }
 
